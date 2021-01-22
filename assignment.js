f9ed42a8716074929c3e80bffa8884c38f1ca6fb
akash <infoaj549814@gmail.com>
2021-01-22 14:56:32 +0600

    buget done

--- assignment.js
+++ assignment.js
@@ -1,3 +1,4 @@
+// github link: https://github.com/Ashaduzzman/assignment.js 
 // kilometerToMeter function  start here 
 function kilometerToMeter(kilometer) {
     var meter = kilometer/1000;
@@ -7,3 +8,27 @@ function kilometerToMeter(kilometer) {
 var rangpure = kilometerToMeter(100);
 console.log(rangpure);
 // kilometerToMeter function end here
+
+
+
+// bugetCalculator start here 
+function bugetCalculator(watch){
+    var watch = watch*3;
+    return watch;
+}
+var watch = bugetCalculator (50);
+console.log(watch);
+
+function bugetCalculator(phone){
+    var phone = phone*2;
+    return phone;
+}
+var phone = bugetCalculator (100);
+console.log(phone);
+function bugetCalculator(laptop) {
+    var laptop =laptop*3;
+    return laptop;
+}
+var laptop =bugetCalculator(500);
+console.log(laptop);
+// bugetCalculator end here
