865b094f0c68a22953ab641f189d27c72e854598
akash <infoaj549814@gmail.com>
2021-01-22 14:02:56 +0600

    add 1st one

---
+++ assignment.js
@@ -0,0 +1,9 @@
+// kilometerToMeter function  start here 
+function kilometerToMeter(kilometer) {
+    var meter = kilometer/1000;
+    return meter;
+    
+}
+var rangpure = kilometerToMeter(100);
+console.log(rangpure);
+// kilometerToMeter function end here
